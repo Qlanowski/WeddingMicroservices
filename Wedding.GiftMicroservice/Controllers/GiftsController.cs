ca28cb362d4fb4e85244d0f487f439bd582f10b2
Karol Ulanowski <ulanowskikarol@gmail.com>
2019-07-20 14:45:51 +0200

    Add project files.

---
+++ Wedding.GiftMicroservice/Controllers/GiftsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wedding.GiftMicroservice.Data;
+using Wedding.GiftMicroservice.Data.Model;
+
+namespace Wedding.GiftMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftsController : ControllerBase
+    {
+        private readonly GiftDbContext _context;
+
+        public GiftsController(GiftDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Gifts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Gift>>> GetGifts()
+        {
+            return await _context.Gifts.ToListAsync();
+        }
+
+        // GET: api/Gifts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Gift>> GetGift(int id)
+        {
+            var gift = await _context.Gifts.FindAsync(id);
+
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return gift;
+        }
+
+        // PUT: api/Gifts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGift(int id, Gift gift)
+        {
+            if (id != gift.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gift).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GiftExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Gifts
+        [HttpPost]
+        public async Task<ActionResult<Gift>> PostGift(Gift gift)
+        {
+            _context.Gifts.Add(gift);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGift", new { id = gift.Id }, gift);
+        }
+
+        // DELETE: api/Gifts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Gift>> DeleteGift(int id)
+        {
+            var gift = await _context.Gifts.FindAsync(id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gifts.Remove(gift);
+            await _context.SaveChangesAsync();
+
+            return gift;
+        }
+
+        private bool GiftExists(int id)
+        {
+            return _context.Gifts.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Wedding.GiftMicroservice/Data/GiftDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wedding.GiftMicroservice.Data.Model;
+
+namespace Wedding.GiftMicroservice.Data
+{
+  public class GiftDbContext:DbContext
+  {
+    public GiftDbContext(DbContextOptions<GiftDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Gift> Gifts { get; set; }
+  }
+}
